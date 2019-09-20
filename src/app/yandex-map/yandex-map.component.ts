55108069e603a22d749dfd2149cf57ab048e3eff
Yuriy <yuriy@beamsolutions.com>
2019-09-20 15:53:30 +0300

    first commit

---
+++ src/app/yandex-map/yandex-map.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-yandex-map',
+  templateUrl: './yandex-map.component.html',
+  styleUrls: ['./yandex-map.component.less']
+})
+export class YandexMapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
